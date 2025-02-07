701e8a5bc1a3d3ae2903d0c4197f05a739dc92a1
smallstall001 <cakbj001@gmail.com>
2025-02-07 12:18:19 +0900

    change same file name

--- README.md
+++ README.md
@@ -17,15 +17,19 @@ This extension contributes the following settings:
 
 Users appreciate release notes as you update your extension.
 
-### 0.0.1
+### 0.6.0
 
-Initial release.
-
-### 0.4.0
-
-A pinned tab is now unmoved.
+Changed so that files with the same name can also be moved.
 
 ### 0.5.0
 
 Change activation arg to milliseconds; allow 0 delay. (Move my tab now!)
 Thanks @KDPRoss
+
+### 0.4.0
+
+A pinned tab is now unmoved.
+
+### 0.0.1
+
+Initial release.
--- extension.js
+++ extension.js
@@ -1,49 +1,54 @@
 // @ts-check
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 let timeout, registration;
 
 const getNotPinnedNum = (activeGroup) => {
-  for (let i = 0; i < activeGroup.tabs.length; i ++) {
+  for (let i = 0; i < activeGroup.tabs.length; i++) {
     if (!activeGroup.tabs[i].isPinned) return i;
   }
 };
 
 const getActiveTabNum = ({ activeTab, tabs }) => {
-  for (let i = 0; i < tabs.length; i ++) {
-    if (activeTab.label === tabs[i].label) return i;
+  for (let i = 0; i < tabs.length; i++) {
+    if (tabs[i].input) {
+      if (tabs[i].input.uri === activeTab.input.uri) {
+        return i;
+      }
+    }
   }
 };
 
 const activate = () => {
   let config;
   const setConfiguration = () => {
-    config = vscode.workspace.getConfiguration('auto-arrange-tabs');
+    config = vscode.workspace.getConfiguration("auto-arrange-tabs");
   };
 
-setConfiguration();
+  setConfiguration();
   if (!config) return;
 
   const leftTab = () => {
     if (timeout) clearTimeout(timeout);
 
     timeout = setTimeout(() => {
-        const activeGroup = vscode.window.tabGroups.all.filter((group) => group.isActive)[0];
-        if (!activeGroup) return;
+      const activeGroup = vscode.window.tabGroups.all.filter(
+        (group) => group.isActive
+      )[0];
+      if (!activeGroup) return;
 
-        // Find tab numbers with matching labels.
-        const tabNum = getActiveTabNum(activeGroup);
-        if (tabNum < config.get('fixTabs')) return;
+      // Find tab numbers with matching labels.
+      const tabNum = getActiveTabNum(activeGroup);
+      console.log("tn", tabNum);
+      if (tabNum < config.get("fixTabs")) return;
 
-        const notPinnedNum = getNotPinnedNum(activeGroup);
-        vscode.commands.executeCommand('moveActiveEditor', {
-          by: 'tab',
-          to: 'left',
-          value: tabNum - notPinnedNum,
-        });
-      },
-      config.get('millis'),
-    );
+      const notPinnedNum = getNotPinnedNum(activeGroup);
+      vscode.commands.executeCommand("moveActiveEditor", {
+        by: "tab",
+        to: "left",
+        value: tabNum - notPinnedNum,
+      });
+    }, config.get("millis"));
   };
 
   const setRegistration = () => {
